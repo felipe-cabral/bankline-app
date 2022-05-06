b9aa71635324f666181c702a9e6c762827fdb145
Felipe Cabral <felipeaugustocabral@gmail.com>
2022-05-06 17:13:34 -0300

    Paginas Correntistas e Movimentações finalizadas com Bootstrap
    Paginas Correntistas e Movimentações finalizadas com Bootstrap

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseURL = 'http://localhost:8080';
+const baseUrl = 'http://localhost:8080';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,15 @@ const baseURL = 'http://localhost:8080';
 export class MovimentacaoService {
 
   constructor(private http: HttpClient) { }
+  
   list(): Observable<any> {
-    return this.http.get('${baseURL}/movimentacoes');
+    return this.http.get(`${baseUrl}/movimentacoes`);
   }
+  findByIdConta(idConta:any): Observable<any> {
+    return this.http.get(`${baseUrl}/movimentacoes/${idConta}`);
+  }
+  create(movimentacao:any): Observable<any> {
+    return this.http.post(`${baseUrl}/movimentacoes`,movimentacao);
+ }
 
 }
